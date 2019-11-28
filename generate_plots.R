ac2752449811ec932e2a028f3c340a835fe9332f
lsouth <lsouth269@gmail.com>
2019-11-28 13:25:39 -0500

    Fix axis labels

--- generate_plots.R
+++ generate_plots.R
@@ -2,26 +2,26 @@ library(ggplot2)
 library(readr)
 t_test <- read_csv("~/neu/courses/cs7290/likert-project/t-test.csv")
 
-sample_size = 30
+treatment_labels_students <- c("Control","Errors not centered at 0", "Non-constant variance in errors",
+                               "Errors from logistic distribution","Dependent observations",
+                               "Discretized data")
+
 ggplot(data=t_test[t_test$sample_size==30,], aes(x=median,y=p_value, color=treatment)) + geom_point() + geom_line() +
   geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
-  ggtitle(expression("P-values from Student's T test "~H["0"]~": "~eta~" = 4 with sample size of 30")) + 
+  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 30")) + 
   theme(plot.title = element_text(hjust = 0.5)) + ylim(0,1) + geom_abline(intercept=0.05,slope=0,linetype="dashed") +
-  scale_color_discrete(name="Treatment",labels=c("Control","Errors not centered at 0", "Non-constant variance in errors",
-                                       "Errors from logistic distribution","Dependent observations","Discretized data"))
+  scale_color_discrete(name="Treatment",labels=treatment_labels_students)
 
 ggplot(data=t_test[t_test$sample_size==100,], aes(x=median,y=p_value, color=treatment)) + geom_point() + geom_line() +
   geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
-  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 30")) +
+  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 100")) +
   theme(plot.title = element_text(hjust = 0.5)) + 
   ylim(0,1) + geom_abline(intercept=0.05,slope=0,linetype="dashed")+
-  scale_color_discrete(name="Treatment",labels=c("Control","Errors not centered at 0", "Non-constant variance in errors",
-                                                 "Errors from logistic distribution","Dependent observations","Discretized data"))
+  scale_color_discrete(name="Treatment",labels=treatment_labels_students)
 
 ggplot(data=t_test[t_test$sample_size==500,], aes(x=median,y=p_value, color=treatment)) + geom_point() + geom_line() +
   geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
-  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 30")) +
+  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 500")) +
   theme(plot.title = element_text(hjust = 0.5)) + 
   ylim(0,1) + geom_abline(intercept=0.05,slope=0,linetype="dashed")+
-  scale_color_discrete(name="Treatment",labels=c("Control","Errors not centered at 0", "Non-constant variance in errors",
-                                                 "Errors from logistic distribution","Dependent observations","Discretized data"))
+  scale_color_discrete(name="Treatment",labels=treatment_labels_students)
