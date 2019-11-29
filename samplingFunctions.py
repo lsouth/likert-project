dca10355a25e570ddb567ca2738307172b4603da
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2019-11-29 14:21:43 -0500

    restrict samples to [1,7]

--- samplingFunctions.py
+++ samplingFunctions.py
@@ -8,23 +8,32 @@ def categorize(sample):
 	sample[sample > 7] = 7
 	return np.floor(sample)
 
-def sampleIndependentNormal(numSamples=100, offset=0):
+def restrict(sample):
+	sample[sample < 1] = 1
+	sample[sample > 7] = 7
+	return sample
+
+def sampleIndependentNormal(numSamples=100, offset=0, binned=False):
 	"""
 	errors are independently from normal distribution N(0,1)
 	"""
-	sample = np.random.normal(loc=offset, scale=1, size=numSamples)
-	return categorize(sample)
+	samples = np.random.normal(loc=offset, scale=1, size=numSamples)
+	if binned:
+		return categorize(samples)
+	return restrict(samples)
 
-def sampleIndpendentContinuousSymmetric(numSamples=100, offset=0):
+def sampleIndpendentContinuousSymmetric(numSamples=100, offset=0, binned=False):
 	"""
 	errors are independent continuous symmetric: mean=median=0
 	Logistic(0,1)
 	"""
 	errors = np.random.logistic(loc=0, scale=1, size=numSamples)
-	sample = offset + errors
-	return categorize(sample)
+	samples = offset + errors
+	if binned:
+		return categorize(samples)
+	return restrict(samples)
 
-def sampleIndependentContinuousAsymmetric(numSamples=100, offset=0):
+def sampleIndependentContinuousAsymmetric(numSamples=100, offset=0, binned=False):
 	"""
 	errors are independent, continuous, median=0, but Asymmetric
 	Gumbel Distribution:
@@ -39,30 +48,36 @@ def sampleIndependentContinuousAsymmetric(numSamples=100, offset=0):
 	beta = 1
 	mu = np.log(np.log(2))
 	errors = stats.gumbel_r.rvs(loc=mu, scale=beta, size=numSamples)
-	sample = offset + errors
-	return categorize(sample)
+	samples = offset + errors
+	if binned:
+		return categorize(samples)
+	return restrict(samples)
 
-def generateDependentSamplesLatentNormal(numSamples=100, numClusters=2):
+def generateDependentSamplesLatentNormal(numSamples=100, offset=0, clusterScale=0.25, numClusters=2, binned=False):
 	"""
 	draw cluster means from normal distribution
 	Then draw samples from each of those clusters
 	"""
-	clusterMeans = np.random.normal(loc=4,scale=1,size = numClusters)
+	clusterMeans = np.random.normal(loc=offset,scale=1,size=numClusters)
 	samples = []
 	for c in clusterMeans:
-	    c_samp = np.random.normal(loc=c,scale=0.25,size=numSamples)
+	    c_samp = np.random.normal(loc=c,scale=clusterScale,size=int(numSamples/numClusters))
 	    samples = np.concatenate((samples,c_samp))
-	return categorize(samples)
+	if binned:
+		return categorize(samples)
+	return restrict(samples)
 
-def generateDependentSamplesLatentLogistic(numSamples=100, numClusters=2):
+def generateDependentSamplesLatentLogistic(numSamples=100, offset=0, numClusters=2, binned=False):
 	"""
 	draw cluster means from normal distribution
 	Then draw samples from each of those clusters
 	"""
-	clusterMeans = np.random.logistic(loc=4,scale=1,size = numClusters)
+	clusterMeans = np.random.logistic(loc=offset,scale=1,size = numClusters)
 	samples = []
 	for c in clusterMeans:
-	    c_samp = np.random.normal(loc=c,scale=0.25,size=numSamples)
+	    c_samp = np.random.normal(loc=c,scale=0.25,size=numSamples / numClusters)
 	    samples = np.concatenate((samples,c_samp))
-	return categorize(samples)
+	if binned:
+		return categorize(binned)
+	return restrict(samples)
 	    
