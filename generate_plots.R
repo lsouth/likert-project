bb16af7aa558ee44c190cd8f0c05bf93ebe65f64
lsouth <lsouth269@gmail.com>
2019-11-29 11:28:45 -0500

    Simplify plotting function

--- generate_plots.R
+++ generate_plots.R
@@ -1,27 +1,36 @@
 library(ggplot2)
 library(readr)
+library(latex2exp)
+
 t_test <- read_csv("~/neu/courses/cs7290/likert-project/t-test.csv")
 
 treatment_labels_students <- c("Control","Errors not centered at 0", "Non-constant variance in errors",
                                "Errors from logistic distribution","Dependent observations",
                                "Discretized data")
 
-ggplot(data=t_test[t_test$sample_size==30,], aes(x=median,y=p_value, color=treatment)) + geom_point() + geom_line() +
-  geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
-  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 30")) + 
-  theme(plot.title = element_text(hjust = 0.5)) + ylim(0,1) + geom_abline(intercept=0.05,slope=0,linetype="dashed") +
-  scale_color_discrete(name="Treatment",labels=treatment_labels_students)
+test_name = "Student T test"
 
-ggplot(data=t_test[t_test$sample_size==100,], aes(x=median,y=p_value, color=treatment)) + geom_point() + geom_line() +
-  geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
-  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 100")) +
-  theme(plot.title = element_text(hjust = 0.5)) + 
-  ylim(0,1) + geom_abline(intercept=0.05,slope=0,linetype="dashed")+
-  scale_color_discrete(name="Treatment",labels=treatment_labels_students)
+make_plot <- function(test_data, test_name, labels, sample_size){
+  ggplot(data=test_data[test_data$sample_size==sample_size,], aes(x=median,y=p_value, color=treatment)) + 
+    geom_point() + geom_line() + geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
+    ggtitle(TeX(paste("P-values from ", test_name, " (H_0: eta = 4) with sample size of ", sample_size))) +
+    theme(plot.title = element_text(hjust = 0.5)) + geom_abline(intercept=0.05,slope=0,linetype="dashed")+
+    scale_color_discrete(name="Treatment",labels=labels) +
+    scale_y_continuous(name="P-value",limits = c(0,1)) + 
+    scale_x_continuous(name=expression(eta))
+}
 
-ggplot(data=t_test[t_test$sample_size==500,], aes(x=median,y=p_value, color=treatment)) + geom_point() + geom_line() +
-  geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
-  ggtitle(expression("P-values from Student's T test ("~H["0"]~": "~eta~" = 4) with sample size of 500")) +
-  theme(plot.title = element_text(hjust = 0.5)) + 
-  ylim(0,1) + geom_abline(intercept=0.05,slope=0,linetype="dashed")+
-  scale_color_discrete(name="Treatment",labels=treatment_labels_students)
+make_treatment_plot <- function(test_data, test_name, sample_size, treatment){
+  ggplot(data=test_data[test_data$sample_size==sample_size & test_data$treatment == treatment,], aes(x=median,y=p_value, color=treatment)) + 
+    geom_point() + geom_line() + geom_errorbar(aes(ymin=lower,ymax=upper,color=treatment,width=0.1)) + 
+    ggtitle(TeX(paste("P-values from ", test_name, " (H_0: eta = 4) with sample size of ", sample_size, " - ", treatment))) +
+    theme(plot.title = element_text(hjust = 0.5)) + geom_abline(intercept=0.05,slope=0,linetype="dashed")+
+    scale_color_discrete(name="Treatment",labels=labels) +
+    scale_y_continuous(name="P-value",limits = c(0,1)) + 
+    scale_x_continuous(name=expression(eta))
+}
+
+make_plot(t_test, test_name, treatment_labels_students, 30)
+make_plot(t_test, test_name, treatment_labels_students, 100)
+make_plot(t_test, test_name, treatment_labels_students, 500)
+
